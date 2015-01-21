4f177626eea147a53c682b241ddcabb7e5aebf85
pospile <pospichalvojtech@sssvt.cz>
2015-01-21 20:41:29 +0100

    update 1.0.1
    login update

--- shop/js/shop.js
+++ shop/js/shop.js
@@ -51,36 +51,6 @@ if (Meteor.isServer)
 					description: 'Další pokračování již dobře známé série se konceptem vrací ke kořenům otevřeného dílu San Andreas, kde jste měli možnost kromě města cestovat také přilehlými oblastmi venkova. Ve hře se ujmete rovnou tří hlavních hrdinů. Michaela, Trevora a Frankieho.'
 				}
 			);
-			Products.insert
-			(
-				{
-					product: 'Minecraft: PC',
-					id: 4,
-					image: 'minecraft.jpg',
-					price: 500,
-					description: 'Další pokračování již dobře známé série se konceptem vrací ke kořenům otevřeného dílu San Andreas, kde jste měli možnost kromě města cestovat také přilehlými oblastmi venkova. Ve hře se ujmete rovnou tří hlavních hrdinů. Michaela, Trevora a Frankieho.'
-				}
-			);
-			Products.insert
-			(
-				{
-					product: 'Minecraft: PS3',
-					id: 4,
-					image: 'minecraft.jpg',
-					price: 800,
-					description: 'Další pokračování již dobře známé série se konceptem vrací ke kořenům otevřeného dílu San Andreas, kde jste měli možnost kromě města cestovat také přilehlými oblastmi venkova. Ve hře se ujmete rovnou tří hlavních hrdinů. Michaela, Trevora a Frankieho.'
-				}
-			);
-			Products.insert
-			(
-				{
-					product: 'Minecraft: Xbox360',
-					id: 4,
-					image: 'minecraft.jpg',
-					price: 850,
-					description: 'Další pokračování již dobře známé série se konceptem vrací ke kořenům otevřeného dílu San Andreas, kde jste měli možnost kromě města cestovat také přilehlými oblastmi venkova. Ve hře se ujmete rovnou tří hlavních hrdinů. Michaela, Trevora a Frankieho.'
-				}
-			);
 		}
 	);
 }
