97e4bf63b1061cd5aaecde7ee65503f14ed0c593
pospile <pospichalvojtech@sssvt.cz>
2015-01-23 14:13:53 +0100

    update 1.0.3
    Hot-fix - uživatelské účty

--- shop/js/secure.js
+++ shop/js/secure.js
@@ -5,11 +5,18 @@ if (Meteor.isServer) {
 		{
 			'create_Acc': function (mail, pass, token)
 			{
-				console.log('Registrace přijate. ' + mail + ' ' + pass);
-				var id = Accounts.createUser({email: mail, password: pass});
-				console.log(id);
-				console.log(token);
-				Alert.insert({tok: token, user: id});
+				if (Meteor.users.find({ emails: { $elemMatch: { address: mail } } }).fetch() == '')
+				{
+					console.log('Registrace přijate. ' + mail + ' ' + pass);
+					var id = Accounts.createUser({email: mail, password: pass});
+					console.log(id);
+					console.log(token);
+					Alert.insert({tok: token, user: id});
+				}
+				else
+				{
+					console.log('Pokus o registraci existujícího uživatele.')
+				}
 			}
 		}
 	)
